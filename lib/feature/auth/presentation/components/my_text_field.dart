4a8ef20e26396e9036b43c254fcd056e6c2cb634
Granthik Som <granthiksom@gmail.com>
2026-03-07 20:20:45 +0530

    daily

--- lib/feature/auth/presentation/components/my_text_field.dart
+++ lib/feature/auth/presentation/components/my_text_field.dart
@@ -6,6 +6,18 @@ class MyTextField extends StatelessWidget {
   final bool obscureText;
 
   const MyTextField({
-    
-  })
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(),
+    );
+  }
 }
